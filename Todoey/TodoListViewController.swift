d35e42113fa7e0c72c7c1c5d8bc8d1b38b17db5c
Abdulrazzaq Alzayed <alzayed98@hotmail.com>
2018-10-05 07:48:26 +0300

    Data persistent user default

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,8 +12,14 @@ class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
 
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
         
         let tempImageView = UIImageView(image: UIImage(named: "AlzayedPrayerTimesbackground"))
         tempImageView.frame = self.tableView.frame
@@ -51,7 +57,11 @@ class TodoListViewController: UITableViewController {
             print("Success!")
             print(textField.text!)
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
+            
         }
         
         alert.addTextField { (alertTextField) in
